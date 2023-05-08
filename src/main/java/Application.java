bfbee1ca644d9d9492436ea07e20ca73b0c38231
endlessmomo <soohyuk96@naver.com>
2023-05-08 15:22:28 +0900

    fix : 잘못된 값에 반환으로 인한 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,10 @@ import global.controller.Controller;
 import global.controller.MapperController;
 import global.controller.command.ControllerMapper;
 import global.util.Print;
+import member.controller.MemberController;
+import member.domain.service.MemberService;
+
+import static global.controller.command.ControllerMapperCommand.LOGIN;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,12 +17,15 @@ public class Application {
     private static ControllerMapper controllerMapper() {
         ControllerMapper mapper = new ControllerMapper();
 
-        /*
-        mapper.put(LOGIN, new LoginController(loginService()));
-        mapper.put(ACCOUNT, new AccountController());
+        mapper.put(LOGIN, new MemberController(memberService()));
+        /*mapper.put(ACCOUNT, new AccountController());
         mapper.put(LOAN, new LoanController());
         */
 
-        return new ControllerMapper();
+        return mapper;
+    }
+
+    private static MemberService memberService() {
+        return new MemberService();
     }
 }
